bd99a84b5f54406a42afea056fb73a56a1300cd2
Andrew Doane <andrew.doane@gmail.com>
2017-05-04 10:30:36 -0300

    Adding PriorityQueue implementation and tests.

---
+++ Algorithms/Collections/PriorityQueue.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.Collections
+{
+    public enum PriorityOrdering
+    {
+        Least,
+        Greatest
+    }
+
+    public class PriorityQueue<Item>
+        where Item : IComparable<Item>
+    {
+        private Item[] heap = new Item[2];
+        private PriorityOrdering ordering;
+
+        public int Count
+        { get; private set; }
+
+        public PriorityQueue(PriorityOrdering ordering = PriorityOrdering.Greatest)
+        {
+            this.ordering = ordering;
+        }
+        
+        public PriorityQueue(int initialCapacity, PriorityOrdering ordering = PriorityOrdering.Greatest)
+            : this(ordering)
+        {
+            heap = new Item[initialCapacity + 1];
+        }
+        
+        public PriorityQueue(ICollection<Item> keys, PriorityOrdering ordering = PriorityOrdering.Greatest)
+            : this(ordering)
+        {
+            heap = new Item[keys.Count + 1];
+            foreach (Item item in keys)
+            {
+                Insert(item);
+            }
+        }
+
+        public void Insert(Item item)
+        {
+            Count++;
+            if (Count >= heap.Length)
+            {
+                Resize(2 * heap.Length);
+            }
+            heap[Count] = item;
+            Swim(Count);
+        }
+
+        public Item Peek()
+        {
+            return heap[1];
+        }
+
+        public Item Pop()
+        {
+            Item top = heap[1];
+            Exchange(1, Count);
+            heap[Count] = default(Item);
+            Count--;
+            Sink(1);
+            if (Count > 0 && Count <= heap.Length / 4)
+            {
+                Resize(heap.Length / 2);
+            }
+            return top;
+        }
+
+        public bool IsEmpty()
+        {
+            return (Count == 0);
+        }
+
+        private bool Less(int i, int j)
+        {
+            switch (ordering)
+            {
+                case PriorityOrdering.Greatest:
+                    return (heap[i].CompareTo(heap[j]) < 0);
+                case PriorityOrdering.Least:
+                    return (heap[i].CompareTo(heap[j]) > 0);
+                default:
+                    return (heap[i].CompareTo(heap[j]) < 0);
+            }
+        }
+
+        private void Exchange(int i, int j)
+        {
+            Item temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;            
+        }
+
+        private void Swim(int k)
+        {
+            while (k > 1 && Less(k/2, k))
+            {
+                Exchange(k / 2, k);
+                k /= 2;
+            }
+        }
+
+        private void Sink(int k)
+        {
+            while (2 * k <= Count)
+            {
+                int j = 2 * k;
+                if (j < Count && Less(j, j + 1))
+                {
+                    j++;
+                }
+                if (!Less(k, j))
+                {
+                    break;
+                }
+                Exchange(k, j);
+                k = j;
+            }
+        }        
+
+        private void Resize(int capacity)
+        {
+            Item[] newHeap = new Item[capacity];
+            for (int i = 0; i <= Count; i++)
+            {
+                newHeap[i] = heap[i];
+            }
+            heap = newHeap;
+        }
+    }
+}
---
+++ Tests/Collections/PriorityQueueTest.cs
@@ -0,0 +1,58 @@
+﻿using Algorithms.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Collections
+{
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        [TestMethod]
+        public void PriorityQueueCreation()
+        {
+            PriorityQueue<int> intPQ = new PriorityQueue<int>();
+            Assert.IsTrue(intPQ.Count == 0);
+            Assert.IsTrue(intPQ.IsEmpty());
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // The default priority ordering is Greatest, so we should get back the largest
+            // element in Peek()
+            intPQ = new PriorityQueue<int>(numbers);
+            Assert.IsTrue(intPQ.Peek() == 5);
+
+            // Test using the Least priority ordering.
+            intPQ = new PriorityQueue<int>(numbers, PriorityOrdering.Least);
+            Assert.IsTrue(intPQ.Peek() == 1);
+
+            PriorityQueue<string> stringPQ = new PriorityQueue<string>(50);
+            Assert.IsTrue(stringPQ.Count == 0);
+            Assert.IsTrue(stringPQ.IsEmpty());
+        }
+
+        [TestMethod]
+        public void PriorityQueueOrdering()
+        {
+            string[] strings = new string[] { "A", "B", "C", "D", "E" };
+
+            // Iterate through using Greatest ordering
+            PriorityQueue<string> maxPQ = new PriorityQueue<string>(strings, PriorityOrdering.Greatest);
+            int index = strings.Length - 1;
+            while (!maxPQ.IsEmpty())
+            {
+                string max = maxPQ.Pop();
+                Assert.AreEqual(strings[index], max);
+                index--;
+            }
+
+            // Iterate through using Least ordering
+            PriorityQueue<string> minPQ = new PriorityQueue<string>(strings, PriorityOrdering.Least);
+            index = 0;
+            while (!minPQ.IsEmpty())
+            {
+                string min = minPQ.Pop();
+                Assert.AreEqual(strings[index], min);
+                index++;
+            }
+        }
+    }
+}
